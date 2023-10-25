fdd68cab95246b216ee660c94a16ce7464ae00b8
rago7 <gopi.edu7@gmail.com>
2023-10-25 15:14:09 -0400

    added POST req processing in views.py

--- ecet_chatbot/landing/views.py
+++ ecet_chatbot/landing/views.py
@@ -3,7 +3,16 @@ from django.shortcuts import render
 HttpResponse
 # Create your views here.
 def simpleLanding(req):
+    # Delete this view as this is no longer in use...
     return HttpResponse("<h1>Hello !!! Landing Page !!</h1>")
 
 def startPage(req):
-    return render(req, 'landing/landing.html')
+    data = {'q':'qqq'}
+    context = {'q0':'Hello User! How may I assist you today !', 'data':data}
+    if req.method == 'POST':
+        # print(req.POST['search_query'])
+        if req.POST['search_query']:
+            data = {'q':req.POST['search_query']}
+        return render(req, 'landing/landing.html', context)
+    else:
+        return render(req, 'landing/landing.html', context)
