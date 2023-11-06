90f1c7c48327ef07bf25a44ccab243c910b07958
rago7 <gopi.edu7@gmail.com>
2023-11-06 15:54:53 -0500

    Chat code

--- ecet_chatbot/landing/views.py
+++ ecet_chatbot/landing/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
-HttpResponse
 # Create your views here.
 def simpleLanding(req):
     # Delete this view as this is no longer in use...
@@ -10,9 +9,11 @@ def startPage(req):
     data = {'q':'qqq'}
     context = {'q0':'Hello User! How may I assist you today !', 'data':data}
     if req.method == 'POST':
-        # print(req.POST['search_query'])
+        query = req.POST.get('search_query', '')
+        print('Query value:: ',query)
         if req.POST['search_query']:
             data = {'q':req.POST['search_query']}
-        return render(req, 'landing/landing.html', context)
+        # return render(req, 'landing/landing.html', context)
+        return JsonResponse({query:'helloo'})
     else:
         return render(req, 'landing/landing.html', context)
